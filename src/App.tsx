e58bbc9df506db64ba4572ece985ea460ef53d0f
fatCat <lefatcatsociety@hotmail.com>
2024-10-13 15:12:57 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="w-full h-screen grid place-items-center">
+      <h1 className=" text-center text-3xl font-bold underline hover:text-blue-700">
+        Esto es una plantilla de React-typescript-tailwind inicial
+      </h1>
+    </div>
+  );
+}
